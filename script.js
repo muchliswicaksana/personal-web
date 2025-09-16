1931320d1ae63c170bebe6b45697f78a5a1e3f7c
muchliswicaksana <145570502+muchliswicaksana@users.noreply.github.com>
2025-09-16 11:03:32 +0700

    menambahkan portfolio section

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ window.addEventListener("scroll", () => {
   const navbar = document.getElementById("navbar");
   navbar.classList.toggle("scrolled", window.scrollY > 0);
 });
+
+const switchTab = (tabId) => {
+  document.querySelectorAll(".tab").forEach(tab => {
+    tab.classList.remove("active");
+  });
+  event.target.classList.add("active");
+
+  document.querySelectorAll(".content").forEach(content => {
+    content.classList.remove("active");
+  });
+  document.getElementById(tabId).classList.add("active");
+};
